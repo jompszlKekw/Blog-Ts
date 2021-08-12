7a5ef58d609daecf0d009807ecb6233714bcaefa
jompszlKekw <74154785+jompszlKekw@users.noreply.github.com>
2021-08-12 20:12:29 -0300

    Login with Facebook

--- src/components/auth/SocialLogin.tsx
+++ src/components/auth/SocialLogin.tsx
@@ -1,25 +1,42 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+
+import {
+  FacebookLogin,
+  FacebookLoginAuthResponse,
+} from "react-facebook-login-lite";
 import { GoogleLogin, GoogleLoginResponse } from "react-google-login-lite";
 
-import { googleLogin } from "../../redux/actions/authAction";
+import { googleLogin, facebookLogin } from "../../redux/actions/authAction";
 
 function SocialLogin() {
   const dispatch = useDispatch();
 
-  const onSuccess = (googleUser: GoogleLoginResponse) => {
+  const onGGSuccess = (googleUser: GoogleLoginResponse) => {
     const id_token = googleUser.getAuthResponse().id_token;
     dispatch(googleLogin(id_token));
   };
 
+  const onFBSuccess = (response: FacebookLoginAuthResponse) => {
+    const { accessToken, userID } = response.authResponse;
+    console.log(response);
+    dispatch(facebookLogin(accessToken, userID));
+  };
+
   return (
-    <div className="my-2">
-      <GoogleLogin
-        client_id="723091898307-rjbo16sof1r89652blrvqr6e4f9eprl8.apps.googleusercontent.com"
-        cookiepolicy="single_host_origin"
-        onSuccess={onSuccess}
-      />
-    </div>
+    <>
+      <div className="my-2">
+        <GoogleLogin
+          client_id="723091898307-rjbo16sof1r89652blrvqr6e4f9eprl8.apps.googleusercontent.com"
+          cookiepolicy="single_host_origin"
+          onSuccess={onGGSuccess}
+        />
+      </div>
+
+      <div className="my-2">
+        <FacebookLogin appId="528588991698145" onSuccess={onFBSuccess} />
+      </div>
+    </>
   );
 }
 
--- src/redux/actions/authAction.ts
+++ src/redux/actions/authAction.ts
@@ -96,3 +96,23 @@ export const googleLogin =
       dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
     }
   };
+
+  export const facebookLogin =
+  (accessToken: string, userID: string) =>
+  async (dispatch: Dispatch<IAuthType | IAlertType>) => {
+    try {
+      dispatch({ type: ALERT, payload: { loading: true } });
+
+      const res = await postAPI("facebook_login", { accessToken, userID });
+
+      dispatch({
+        type: AUTH,
+        payload: res.data,
+      });
+
+      dispatch({ type: ALERT, payload: { success: res.data.msg } });
+      localStorage.setItem('logged', 'true')
+    } catch (err: any) {
+      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+    }
+  };
