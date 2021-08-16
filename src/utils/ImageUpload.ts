3d205096f35496cbfe399c03cd8a7e680a447c36
jompszlKekw <88796528+jompszlKekw@users.noreply.github.com>
2021-08-16 10:18:04 -0300

    Update ImageUpload.ts

--- src/utils/ImageUpload.ts
+++ src/utils/ImageUpload.ts
@@ -12,11 +12,11 @@ export const checkImage = (file: File) => {
 export const imageUpload = async (file: File) => {
   const formData = new FormData();
   formData.append("file", file);
-  formData.append("upload_preset", "xwqohnif");
-  formData.append("cloud_name", "devat-channel");
+  formData.append("upload_preset", "");
+  formData.append("cloud_name", "");
 
   const res = await fetch(
-    "https://api.cloudinary.com/v1_1/devat-channel/upload",
+    "",
     {
       method: "POST",
       body: formData,
