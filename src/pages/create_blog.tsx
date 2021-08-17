77af49f14a880a1b652bfd3a8cc883ad5bfa88b0
jompszlKekw <74154785+jompszlKekw@users.noreply.github.com>
2021-08-17 15:54:57 -0300

    Create Blog 3

--- src/pages/create_blog.tsx
+++ src/pages/create_blog.tsx
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { RootStore, IBlog } from "../utils/TypeScript";
+import { validCreateBlog } from "../utils/Valid";
 
 import NotFound from "../components/global/NotFound";
 import CreateForm from "../components/cards/CreateForm";
 import CardHoriz from "../components/cards/CardHoriz";
 
 import ReactQuill from "../components/editor/ReactQuill";
+
+import { ALERT } from "../redux/types/alertType";
+
+import { createBlog } from "../redux/actions/blogAction";
 
 const CreateBlog = () => {
   const initState = {
@@ -23,8 +28,32 @@ const CreateBlog = () => {
   const [blog, setBlog] = useState<IBlog>(initState);
   const [body, setBody] = useState("");
 
-  const { auth, categories } = useSelector((state: RootStore) => state);
+  const divRef = useRef<HTMLDivElement>(null);
+  const [text, setText] = useState("");
+
+  const { auth } = useSelector((state: RootStore) => state);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const div = divRef.current;
+    if (!div) return;
+
+    const text = div?.innerText as string;
+    setText(text);
+  }, [body]);
+
+  const handleSubmit = async () => {
+    if (!auth.access_token) return;
+
+    const check = validCreateBlog({ ...blog, content: text });
+    if (check.errLength !== 0) {
+      return dispatch({ type: ALERT, payload: { errors: check.errMsg } });
+    }
+
+    let newData = { ...blog, content: body };
+
+    dispatch(createBlog(newData, auth.access_token));
+  };
 
   if (!auth.access_token) return <NotFound />;
   return (
@@ -43,7 +72,22 @@ const CreateBlog = () => {
 
       <ReactQuill setBody={setBody} />
 
-      <button className="btn btn-dark mt-3 d-block mx-auto">Create Post</button>
+      <div
+        ref={divRef}
+        dangerouslySetInnerHTML={{
+          __html: body,
+        }}
+        style={{ display: "none" }}
+      />
+
+      <small>{text.length}</small>
+
+      <button
+        className="btn btn-dark mt-3 d-block mx-auto"
+        onClick={handleSubmit}
+      >
+        Create Post
+      </button>
     </div>
   );
 };
--- src/utils/Valid.ts
+++ src/utils/Valid.ts
@@ -1,4 +1,4 @@
-import { IUserRegister } from "./TypeScript";
+import { IBlog, IUserRegister } from "./TypeScript";
 
 export function ValidRegister(userRegister: IUserRegister) {
   const { name, account, password, cf_password } = userRegister;
@@ -48,4 +48,43 @@ export function validateEmail(email: string) {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
+}
+
+export function validCreateBlog({
+  title,
+  content,
+  description,
+  thumbnail,
+  category,
+}: IBlog) {
+  const err: string[] = [];
+
+  if (title.trim().length < 10) {
+    err.push("Title has at least 10 characters.");
+  } else if (title.trim().length > 50) {
+    err.push("Title is up to 10 characters long.");
+  }
+
+  if (content.trim().length < 500) {
+    err.push("Content has at least 10 characters.");
+  }
+
+  if (description.trim().length < 20) {
+    err.push("Description has at least 20 characters.");
+  } else if (description.trim().length > 200) {
+    err.push("description is up to 0 characters long.");
+  }
+
+  if (!thumbnail) {
+    err.push("Thumbnail cannot be left blank.");
+  }
+
+  if (!category) {
+    err.push("Category cannot be left blank.");
+  }
+
+  return {
+    errMsg: err,
+    errLength: err.length,
+  };
 }
