74f25492bbdbadfe7065a615a13ee83626e66948
jompszlKekw <74154785+jompszlKekw@users.noreply.github.com>
2021-08-19 16:55:58 -0300

    Details Blog

---
+++ src/components/blog/DisplayBlog.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { IBlog } from "../../utils/TypeScript";
+
+interface IProps {
+  blog: IBlog;
+}
+
+const DisplayBlog: React.FC<IProps> = ({ blog }) => {
+  return (
+    <div className="">
+      <h2
+        className="text-center my-3 text-capitalize fs-1 "
+        style={{ color: "#ff7a00" }}
+      >
+        {blog.title}
+      </h2>
+
+      <div className="text-end" style={{ color: "teal" }}>
+        <small>
+          {typeof blog.user !== "string" && `By: ${blog.user.name}`}
+        </small>
+
+        <small className="ms-2">
+          {new Date(blog.createdAt).toLocaleDateString()}
+        </small>
+      </div>
+
+      <div
+        dangerouslySetInnerHTML={{
+          __html: blog.content,
+        }}
+      />
+    </div>
+  );
+};
+
+export default DisplayBlog;
---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { getAPI } from "../../utils/FetchData";
+
+import { IBlog, IParams } from "../../utils/TypeScript";
+
+import Loading from "../../components/global/Loading";
+import { showErrMsg } from "../../components/alert/Alert";
+import DisplayBlog from "../../components/blog/DisplayBlog";
+
+function DetailBlog() {
+  const id = useParams<IParams>().slug;
+
+  const [blog, setBlog] = useState<IBlog>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (!id) return;
+    setLoading(true);
+    getAPI(`blog/${id}`)
+      .then((res) => {
+        setBlog(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.response.data.msg);
+        setLoading(false);
+      });
+
+    return () => setBlog(undefined);
+  }, [id]);
+
+  if (loading) return <Loading />;
+  return (
+    <div className="my-4">
+      {error && showErrMsg(error)}
+
+      {blog && <DisplayBlog blog={blog} />}
+    </div>
+  );
+}
+
+export default DetailBlog;
