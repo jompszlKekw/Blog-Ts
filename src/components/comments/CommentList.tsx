4a799ffd9862e7b8f72647c9e243249ef39b79dc
jompszlKekw <74154785+jompszlKekw@users.noreply.github.com>
2021-08-28 17:19:00 -0300

    Reply comments

--- src/components/comments/CommentList.tsx
+++ src/components/comments/CommentList.tsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { IComment } from "../../utils/TypeScript";
+import { IComment, RootStore } from "../../utils/TypeScript";
 
+import Input from "./Input";
 interface IProps {
   comment: IComment;
+  showReply: IComment[];
+  setShowReply: (showReply: IComment[]) => void;
 }
 
-const CommentList: React.FC<IProps> = ({ comment }) => {
+const CommentList: React.FC<IProps> = ({
+  comment,
+  showReply,
+  setShowReply,
+  children,
+}) => {
+  const [onReply, setOnReply] = useState(false);
+  const { auth } = useSelector((state: RootStore) => state);
+  const dispatch = useDispatch();
+
+  const handleReply = (body: string) => {
+    if (!auth.user || !auth.access_token) return;
+
+    const data = {
+      user: auth.user,
+      blog_id: comment.blog_id,
+      blog_user_id: comment.blog_user_id,
+      content: body,
+      reply_user: comment.user,
+      comment_root: comment._id,
+      createdAt: new Date().toLocaleDateString(),
+    };
+
+    setShowReply([...showReply, data]);
+    setOnReply(false);
+  };
+
   return (
     <div className="w-100">
       <div className="comment_box">
@@ -18,11 +48,20 @@ const CommentList: React.FC<IProps> = ({ comment }) => {
         />
 
         <div className="d-flex justify-content-between p-2">
-          <small style={{ cursor: "pointer" }}>- Reply -</small>
+          <small
+            style={{ cursor: "pointer" }}
+            onClick={() => setOnReply(!onReply)}
+          >
+            {onReply ? "- Cancel -" : "- Reply -"}
+          </small>
 
           <small>{new Date(comment.createdAt).toLocaleString()}</small>
         </div>
       </div>
+
+      {onReply && <Input callback={handleReply} />}
+
+      {children}
     </div>
   );
 };
--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { IComment } from "../../utils/TypeScript";
 
 import AvatarComment from "./AvatarComment";
+import AvatarReply from "./AvatarReply";
 import CommentList from "./CommentList";
 
 interface IProps {
@@ -10,6 +11,8 @@ interface IProps {
 }
 
 const Comments: React.FC<IProps> = ({ comment }) => {
+  const [showReply, setShowReply] = useState<IComment[]>([]);
+
   return (
     <div
       className="my-3 d-flex"
@@ -19,7 +22,28 @@ const Comments: React.FC<IProps> = ({ comment }) => {
       }}
     >
       <AvatarComment user={comment.user} />
-      <CommentList comment={comment} />
+      <CommentList
+        comment={comment}
+        showReply={showReply}
+        setShowReply={setShowReply}
+      >
+        {showReply.map((comment, index) => (
+          <div
+            key={index}
+            style={{
+              opacity: comment._id ? 1 : 0.5,
+              pointerEvents: comment._id ? "initial" : "none",
+            }}
+          >
+            <AvatarReply user={comment.user} reply_user={comment.reply_user} />
+            <CommentList
+              comment={comment}
+              showReply={showReply}
+              setShowReply={setShowReply}
+            />
+          </div>
+        ))}
+      </CommentList>
     </div>
   );
 };
